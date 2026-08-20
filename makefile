058eee631fd545fd86e52f8be05ef2999efddf29
Gabriel Malta <ggm@cesar.school>
2026-08-20 09:50:45 -0300

    implementando o makefile, e implementando a função fork() e exec().

--- makefile
+++ makefile
@@ -1,8 +1,10 @@
-CC = gcc
-CFLAGS = -Wall -Wextra -std=c11
+processflow/processflow: processflow/main.o
+	gcc processflow/main.o -o processflow/processflow
 
-processflow/processflow: processflow/src/main.c
-	$(CC) $(CFLAGS) processflow/src/main.c -o processflow/processflow
+processflow/main.o: processflow/src/main.c 
+	gcc -c processflow/src/main.c -o processflow/main.o
 
 clean:
-	rm -f processflow/processflow
+	rm -f processflow/*.o processflow/processflow
+
+.PHONY: clean
--- processflow/src/main.c
+++ processflow/src/main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_TASK 20
+
+typedef struct {
+    char nome[50];
+    char programa[100];
+} Task;
+
 int main() {
-    char comando[100];
+    Task tasks[MAX_TASK];
+    int total = 0;
+    char comando[200];
     while (1) {
         printf("processflow> ");
         fflush(stdout);
-        if (fgets(comando, sizeof(comando), stdin) == NULL) {
-            break;
-        }
+        fgets(comando, sizeof(comando), stdin);
         comando[strcspn(comando, "\n")] = '\0';
-
         if (strcmp(comando, "exit") == 0) {
             break;
+        }
+        if (strncmp(comando, "task ", 5) == 0) {
+            char nome[50];
+            char programa[100];
+            if (sscanf(comando, "task %49s %99s", nome, programa) != 2) { //Ler uma palavra de até 49 caracteres. Tem que deixar 1 posição para o \0
+                printf("Erro: use task <nome> <programa>\n");
+                continue;
+            }
+            strcpy(tasks[total].nome, nome);
+            strcpy(tasks[total].programa, programa);
+            total++;
+            printf("Tarefa cadastrada: %s\n", nome);
         }
     }
     return 0;
