72c7a1596248fbac0690b34b619d47ed60da699f
Gabriel Malta <ggm@cesar.school>
2026-08-21 10:41:13 -0300

    Implementando função run, para rodar as taks

--- processflow/src/main.c
+++ processflow/src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define MAX_TASK 20
+#define MAX_TASKS 20
 
 typedef struct {
     char nome[50];
@@ -9,13 +11,15 @@ typedef struct {
 } Task;
 
 int main() {
-    Task tasks[MAX_TASK];
+    Task tasks[MAX_TASKS];
     int total = 0;
     char comando[200];
     while (1) {
         printf("processflow> ");
         fflush(stdout);
-        fgets(comando, sizeof(comando), stdin);
+        if (fgets(comando, sizeof(comando), stdin) == NULL) {
+            break;
+        }
         comando[strcspn(comando, "\n")] = '\0';
         if (strcmp(comando, "exit") == 0) {
             break;
@@ -23,14 +27,42 @@ int main() {
         if (strncmp(comando, "task ", 5) == 0) {
             char nome[50];
             char programa[100];
-            if (sscanf(comando, "task %49s %99s", nome, programa) != 2) { //Ler uma palavra de até 49 caracteres. Tem que deixar 1 posição para o \0
+            if (sscanf(comando, "task %49s %99s", nome, programa) != 2) {
                 printf("Erro: use task <nome> <programa>\n");
                 continue;
             }
             strcpy(tasks[total].nome, nome);
             strcpy(tasks[total].programa, programa);
             total++;
-            printf("Tarefa cadastrada: %s\n", nome);
+            printf("Task cadastrada: %s\n", nome);
+        }
+        else if (strncmp(comando, "run ", 4) == 0) {
+            char nome[50];
+            int encontrada = 0;
+            sscanf(comando, "run %49s", nome);
+            for (int i = 0; i < total; i++) {
+                if (strcmp(tasks[i].nome, nome) == 0) {
+                    encontrada = 1;
+                    pid_t pid = fork();
+                    if (pid < 0) {
+                        printf("Erro ao criar processo.\n");
+                        break;
+                    }
+                    if (pid == 0) {
+                        execl(tasks[i].programa,
+                              tasks[i].nome,
+                              NULL);
+
+                        printf("Erro: nao foi possivel executar a tarefa.\n");
+                        return 1;
+                    }
+                    waitpid(pid, NULL, 0);
+                    break;
+                }
+            }
+            if (!encontrada) { //não encontrei a task
+                printf("Erro: tarefa nao encontrada.\n");
+            }
         }
     }
     return 0;
