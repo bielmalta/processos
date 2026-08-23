21939f8b08dbe63ad1212e0c9ce230f25cdf3067
Gabriel Malta <ggm@cesar.school>
2026-08-23 11:28:12 -0300

    implementando sequencial e parallel

--- processflow/src/main.c
+++ processflow/src/main.c
@@ -13,6 +13,74 @@ typedef struct {
     int total_argumentos;
 } Task;
 
+int registrar_task(Task tasks[], int total, char comando[]) {
+    char copia[200];
+    char *nome;
+    char *programa;
+    char *argumento;
+
+    strcpy(copia, comando);
+
+    strtok(copia, " ");          // ignora a palavra "task"
+    nome = strtok(NULL, " ");
+    programa = strtok(NULL, " ");
+
+    if (nome == NULL || programa == NULL) {
+        printf("Erro: use task <nome> <programa> " "[argumentos...]\n");
+        return 0;
+    }
+    snprintf(tasks[total].nome, sizeof(tasks[total].nome), "%s", nome);
+
+    snprintf(tasks[total].programa, sizeof(tasks[total].programa), "%s",programa);
+    tasks[total].total_argumentos = 0;
+
+    argumento = strtok(NULL, " ");
+    while (
+        argumento != NULL &&
+        tasks[total].total_argumentos < MAX_ARGUMENTOS
+    ) {
+        int posicao = tasks[total].total_argumentos;
+
+        snprintf(tasks[total].argumentos[posicao], sizeof(tasks[total].argumentos[posicao]), "%s", argumento);
+        tasks[total].total_argumentos++;
+        argumento = strtok(NULL, " ");
+    }
+    return 1;
+}
+
+int run_task(Task tasks[], int total, char nome[]) {
+    int indice = -1; //O -1 significa que nenhuma tarefa foi encontrada.
+    for (int i = 0; i < total; i++) {
+        if (strcmp(tasks[i].nome, nome) == 0) {
+            indice = i;
+            break;
+        }
+    }
+    if (indice == -1) {
+        printf("Erro: tarefa %s nao encontrada.\n", nome);
+        return 0;
+    }
+    pid_t pid = fork(); //criei o processo filho
+    if (pid < 0) {
+        printf("Erro ao criar processo.\n"); 
+        return 0;
+    }
+    if (pid == 0) {
+        char *argumentos_exec[MAX_ARGUMENTOS + 2];
+        argumentos_exec[0] = tasks[indice].programa;
+        for (int j = 0; j < tasks[indice].total_argumentos; j++) {
+            argumentos_exec[j + 1] = tasks[indice].argumentos[j]; 
+        }
+        argumentos_exec[tasks[indice].total_argumentos + 1] = NULL;
+        execv(tasks[indice].programa, argumentos_exec); //faz o filho executar o programa.
+        printf("Erro: nao foi possivel executar a tarefa.\n");
+        fflush(stdout);
+        _exit(1); //encerra o processo filho mostrando que deu erro
+    }
+    waitpid(pid, NULL, 0); //processo pai espera o processo filho terminar
+    return 1;
+}
+
 int main() {
     Task tasks[MAX_TASKS];
     int total = 0;
@@ -29,68 +97,92 @@ int main() {
             break;
         }
         if (strncmp(comando, "task ", 5) == 0) {
-            char nome[50];
-            char programa[100];
-            char texto_argumentos[200] = "";
-            char *argumento;
-            int quantidade = sscanf(
-                comando,
-                "task %49s %99s %199[^\n]",
-                nome,
-                programa,
-                texto_argumentos
-            );
-            if (quantidade < 2) {
-                printf(
-                    "Erro: use task <nome> <programa> [argumentos...]\n"
-                );
-                continue;
+            if (registrar_task(tasks, total, comando) == 1) {
+                printf("Task cadastrada: %s\n", tasks[total].nome);
+                total++;
             }
-            strcpy(tasks[total].nome, nome);
-            strcpy(tasks[total].programa, programa);
-            tasks[total].total_argumentos = 0;
-            if (quantidade == 3) {
-                argumento = strtok(texto_argumentos, " ");
-                while (argumento != NULL && tasks[total].total_argumentos < MAX_ARGUMENTOS) {
-                    int posicao = tasks[total].total_argumentos;
-                    snprintf(tasks[total].argumentos[posicao], sizeof(tasks[total].argumentos[posicao]), "%s",argumento);
-                    tasks[total].total_argumentos++;
-                    argumento = strtok(NULL, " ");
+            
+        } else if (strcmp(comando, "run sequential") == 0) {
+            printf("Erro: use run sequential " "<tarefa1> <tarefa2> ...\n");
+
+        }else if (strncmp(comando, "run sequential ", 15) == 0) {
+            char copia[200];
+            char *nome_task;
+
+            strcpy(copia, comando + 15);
+            nome_task = strtok(copia, " "); //strtok separa os nomes
+            while (nome_task != NULL) {
+                if (run_task(tasks, total, nome_task) == 0) {
+                    break;
                 }
+                nome_task = strtok(NULL, " ");
             }
-            total++;
-            printf("Task cadastrada: %s\n", nome);
+        }
+        else if (strcmp(comando, "run parallel") == 0) {
+            printf("Erro: use run parallel " "<tarefa1> <tarefa2> ...\n");
+
+        }else if (strncmp(comando, "run parallel ", 13) == 0) {
+            char copia[200];
+            char *nome_task;
+            pid_t pids[MAX_TASKS];
+            int total_processos = 0;
+
+            strcpy(copia, comando + 13);
+            nome_task = strtok(copia, " ");
+
+            while (nome_task != NULL) {
+                int indice = -1;
+                for (int i = 0; i < total; i++) {
+                    if (strcmp(tasks[i].nome, nome_task) == 0) {
+                        indice = i;
+                        break;
+                    }
+                }
+                if (indice == -1) {
+                    printf("Erro: tarefa %s nao encontrada.\n", nome_task);
+                    break;
+                }
+                if (total_processos == MAX_TASKS) {
+                    printf("Erro: limite de processos atingido.\n");
+                    break;
+                }
+
+                pid_t pid = fork();
+
+                if (pid < 0) {
+                    printf("Erro ao criar processo.\n");
+                    break;
+                }
+                if (pid == 0) {
+                    char *argumentos_exec[MAX_ARGUMENTOS + 2];
+                    argumentos_exec[0] = tasks[indice].programa;
+                    for (int j = 0; j < tasks[indice].total_argumentos; j++) {
+                        argumentos_exec[j + 1] = tasks[indice].argumentos[j];
+                    }
+
+                    argumentos_exec[tasks[indice].total_argumentos + 1] = NULL;
+
+                    execv(tasks[indice].programa, argumentos_exec);
+                    printf("Erro: nao foi possivel executar " "a tarefa %s.\n", nome_task);
+                    fflush(stdout);
+                    _exit(1);
+                }
+
+                pids[total_processos] = pid;
+                total_processos++;
+                nome_task = strtok(NULL, " ");
+            }
+            for (int i = 0; i < total_processos; i++) {
+                waitpid(pids[i], NULL, 0);
+            }
 
         }else if (strncmp(comando, "run ", 4) == 0) {
             char nome[50];
-            int encontrada = 0;
-            sscanf(comando, "run %49s", nome);
-            for (int i = 0; i < total; i++) {
-                if (strcmp(tasks[i].nome, nome) == 0) {
-                    encontrada = 1;
-                    pid_t pid = fork();
-                    if (pid < 0) {
-                        printf("Erro ao criar processo.\n");
-                        break;
-                    }
-                    if (pid == 0) {
-                        char *argumentos_exec[MAX_ARGUMENTOS + 2];
-                        argumentos_exec[0] = tasks[i].programa;
-                        for (int j = 0; j < tasks[i].total_argumentos; j++) {
-                            argumentos_exec[j + 1] = tasks[i].argumentos[j];
-                        }
-                        argumentos_exec[tasks[i].total_argumentos + 1] = NULL;
-                        execv(tasks[i].programa, argumentos_exec);
-                        printf("Erro: nao foi possivel executar a tarefa.\n");
-                        return 1;
-                    }
-                    waitpid(pid, NULL, 0);
-                    break;
-                }
+            if (sscanf(comando, "run %49s", nome) != 1) {
+                printf("Erro: use run <nome>\n");
+                continue;
             }
-            if (!encontrada) {
-                printf("Erro: tarefa nao encontrada.\n");
-            }
+            run_task(tasks, total, nome);
         }
     }
     return 0;
